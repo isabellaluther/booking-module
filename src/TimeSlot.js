2e68f748c8896e91bdf03b3511ee131b3e186b93
Isabella Luther <il223at@student.lnu.se>
2026-09-18 20:17:14 +0200

    Add TimeSlot class

---
+++ src/TimeSlot.js
@@ -0,0 +1,53 @@
+/**
+ * Contains the TimeSlot class.
+ * 
+ * @file src/TimeSlot.js
+ * @author Isabella Luther <il223at@student.lnu.se>
+ * @version 1.0.0
+ * @license Unlicense
+ */
+
+/**
+ * Represents a time slot with a start and end time.
+ * 
+ * @class TimeSlot
+ */
+export class TimeSlot {
+    /**
+     * The start time of the time slot.
+     */
+    #startTime
+    /**
+     * The end time of the time slot.
+     */
+    #endTime
+
+  /**
+   * Creates a new TimeSlot instance with the specified start and end times.
+   * 
+   * @param {Date} startTime - The start time of the time slot.
+   * @param {Date} endTime - The end time of the time slot.
+   */
+  constructor(startTime, endTime) {
+    this.#startTime = startTime
+    this.#endTime = endTime
+  }
+
+  /**
+   * Gets the start time of the time slot.
+   *
+   * @returns {Date} The start time of the time slot.
+   */
+  getStartTime() {
+    return this.#startTime
+  }
+
+  /**
+   * Gets the end time of the time slot.
+   *
+   * @returns {Date} The end time of the time slot.
+   */
+  getEndTime() {
+    return this.#endTime
+  }
+}
