8a73116767f2572ae74b653a2567dd7a74dbf990
Isabella Luther <il223at@student.lnu.se>
2026-09-18 20:37:57 +0200

    Add test for time slot duration

--- test/TimeSlot.test.js
+++ test/TimeSlot.test.js
@@ -1,6 +1,6 @@
 /**
  * Contains tests for the TimeSlot class.
- * 
+ *
  * @file test/TimeSlot.test.js
  * @author Isabella Luther <il223at@student.lnu.se>
  * @version 1.0.0
@@ -27,11 +27,21 @@ describe('TimeSlot', () => {
 
   // Test that an error is thrown when the start time is after the end time
   it('throws an error when start time is after end time', () => {
-  const startTime = new Date('2026-09-20T12:00:00')
-  const endTime = new Date('2026-09-20T10:00:00')
+    const startTime = new Date('2026-09-20T12:00:00')
+    const endTime = new Date('2026-09-20T10:00:00')
 
-  expect(() => {
-    new TimeSlot(startTime, endTime)
-  }).toThrow()
-})
+    expect(() => {
+      new TimeSlot(startTime, endTime)
+    }).toThrow()
+  })
+
+  // Test that the duration in minutes is calculated correctly
+  it('returns the duration in minutes', () => {
+    const startTime = new Date('2026-09-20T10:00:00')
+    const endTime = new Date('2026-09-20T11:30:00')
+
+    const timeSlot = new TimeSlot(startTime, endTime)
+
+    expect(timeSlot.getDurationInMinutes()).toBe(90)
+  })
 })
