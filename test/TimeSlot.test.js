a3cd837214a51235a25eefde8eac41be14e47703
Isabella Luther <il223at@student.lnu.se>
2026-09-18 20:24:41 +0200

    Add TimeSlot tests

---
+++ test/TimeSlot.test.js
@@ -0,0 +1,26 @@
+/**
+ * Contains tests for the TimeSlot class.
+ * 
+ * @file test/TimeSlot.test.js
+ * @author Isabella Luther <il223at@student.lnu.se>
+ * @version 1.0.0
+ * @license Unlicense
+ */
+
+import { describe, expect, it } from 'vitest'
+import { TimeSlot } from '../src/TimeSlot.js'
+
+/**
+ * Tests for the TimeSlot class.
+ */
+describe('TimeSlot', () => {
+  it('returns the start and end time', () => {
+    const startTime = new Date('2026-09-20T10:00:00')
+    const endTime = new Date('2026-09-20T11:00:00')
+
+    const timeSlot = new TimeSlot(startTime, endTime)
+
+    expect(timeSlot.getStartTime()).toBe(startTime)
+    expect(timeSlot.getEndTime()).toBe(endTime)
+  })
+})
