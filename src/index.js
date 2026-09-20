0bfa14352f609a1e343c7af6967158c96086fa45
Isabella Luther <il223at@student.lnu.se>
2026-09-20 19:12:43 +0200

    Add public module interface

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Public interface for the booking module.
+ *
+ * @file src/index.js
+ * @author Isabella Luther <il223at@student.lnu.se>
+ * @version 1.0.0
+ * @license Unlicense
+ */
+
+export { Booking } from './Booking.js'
+export { BookingCalendar } from './BookingCalendar.js'
+export { Resource } from './Resource.js'
+export { TimeSlot } from './TimeSlot.js'
