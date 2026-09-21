5b6ee0bcf2389a177db83a22a455b1bf855df847
Isabella Luther <il223at@student.lnu.se>
2026-09-21 14:57:56 +0200

    Add test for 30 minute availability around booking

--- test/BookingCalendar.test.js
+++ test/BookingCalendar.test.js
@@ -531,4 +531,36 @@ describe('BookingCalendar', () => {
 
     expect(availableTimeSlots[2].getEndTime()).toEqual(new Date('2026-09-20T10:30:00'))
   })
+
+  // Test that 30 minute time slots are returned correctly around an existing booking.
+  it('returns 30 minute time slots around an existing booking', () => {
+    const calendar = new BookingCalendar()
+    const resource = new Resource('room-101', 'Study Room 101')
+
+    const booking = new Booking(
+      'booking-1',
+      resource,
+      new TimeSlot(new Date('2026-09-20T09:30:00'), new Date('2026-09-20T10:00:00'))
+    )
+
+    calendar.addBooking(booking)
+
+    const searchTimeSlot = new TimeSlot(new Date('2026-09-20T09:00:00'), new Date('2026-09-20T11:00:00'))
+
+    const availableTimeSlots = calendar.getAvailableTimeSlots(resource, searchTimeSlot, 30)
+
+    expect(availableTimeSlots.length).toBe(3)
+
+    expect(availableTimeSlots[0].getStartTime()).toEqual(new Date('2026-09-20T09:00:00'))
+
+    expect(availableTimeSlots[0].getEndTime()).toEqual(new Date('2026-09-20T09:30:00'))
+
+    expect(availableTimeSlots[1].getStartTime()).toEqual(new Date('2026-09-20T10:00:00'))
+
+    expect(availableTimeSlots[1].getEndTime()).toEqual(new Date('2026-09-20T10:30:00'))
+
+    expect(availableTimeSlots[2].getStartTime()).toEqual(new Date('2026-09-20T10:30:00'))
+
+    expect(availableTimeSlots[2].getEndTime()).toEqual(new Date('2026-09-20T11:00:00'))
+  })
 })
